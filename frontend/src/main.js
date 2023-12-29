4c6085ed4abec49cb596a5b2ecac146222cb930a
Rayan Khan <riyanswat@gmail.com>
2023-12-30 01:20:36 +0500

    updated

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -117,6 +117,7 @@ class PasswordManager {
       return;
     }
 
+    // check if password is correct
     const password = await this.getPassword();
     if (password != this.passwordProtection) {
       Swal.fire("Incorrect password");
@@ -146,61 +147,47 @@ class PasswordManager {
     }
   }
 
-  handleDelete() {
+  async handleDelete() {
     const websiteToDelete = this.websiteElement.value;
     if (!websiteToDelete) {
       showAlert(this.alertMessage, "Please enter a website");
       return;
     }
 
-    const { value: password } = Swal.fire({
-      title: "Enter your password",
-      input: "password",
-      inputLabel: "Password",
-      inputPlaceholder: "Enter your password",
-      inputAttributes: {
-        maxlength: "10",
-        autocapitalize: "off",
-        autocorrect: "off",
-      },
-    }).then((password) => {
-      if (password.value != this.passwordProtection) {
-        console.log("==========================", password.value);
-        console.log("==========================", this.passwordProtection);
-        Swal.fire(`Incorrect password`);
-        return;
-      } else {
-        Search(websiteToDelete).then((res) => {
-          if (res[1] == "yes") {
-            //* Confirm deletion
-            Swal.fire({
-              title: "Are you sure?",
-              text: `Do you really want to delete '${websiteToDelete}'?`,
-              icon: "warning",
-              showCancelButton: true,
-              confirmButtonColor: "#d33",
-              cancelButtonColor: "#3085d6",
-              confirmButtonText: "Yes, delete it!",
-            }).then((result) => {
-              if (result.isConfirmed) {
-                //* Deletion:
-                Delete(websiteToDelete).then((res) =>
-                  showAlert(alertMessage, res)
-                );
+    // check if password is correct
+    const password = await this.getPassword();
+    if (password != this.passwordProtection) {
+      Swal.fire("Incorrect password");
+      return;
+    }
 
-                //* Deletion successful
-                Swal.fire({
-                  title: "Deleted!",
-                  text: `'${websiteToDelete}' has been deleted.`,
-                  icon: "success",
-                });
-                this._clearFields();
-              }
+    Search(websiteToDelete).then((res) => {
+      if (res[1] == "yes") {
+        //* Confirm deletion
+        Swal.fire({
+          title: "Are you sure?",
+          text: `Do you really want to delete '${websiteToDelete}'?`,
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#d33",
+          cancelButtonColor: "#3085d6",
+          confirmButtonText: "Yes, delete it!",
+        }).then((result) => {
+          if (result.isConfirmed) {
+            //* Deletion:
+            Delete(websiteToDelete).then((res) => showAlert(alertMessage, res));
+
+            //* Deletion successful
+            Swal.fire({
+              title: "Deleted!",
+              text: `'${websiteToDelete}' has been deleted.`,
+              icon: "success",
             });
-          } else if (res[1] == "no") {
-            showAlert(this.alertMessage, "Website doesn't exist");
+            this._clearFields();
           }
         });
+      } else if (res[1] == "no") {
+        showAlert(this.alertMessage, "Website doesn't exist");
       }
     });
   }
@@ -218,157 +205,120 @@ class PasswordManager {
     showAlert(alertMessage, "Password generated");
   }
 
-  handleShowAll() {
+  async handleShowAll() {
     // const protectionHtml =
     //   '<input id="protect" class="swal2-input" placeholder="Password">';
 
-    const { value: password } = Swal.fire({
-      title: "Enter your password",
-      input: "password",
-      inputLabel: "Password",
-      inputPlaceholder: "Enter your password",
-      inputAttributes: {
-        maxlength: "10",
-        autocapitalize: "off",
-        autocorrect: "off",
-      },
-    }).then((password) => {
-      if (password.value != this.passwordProtection) {
-        console.log("==========================", password.value);
-        console.log("==========================", this.passwordProtection);
-        Swal.fire(`Incorrect password`);
-        return;
-      } else {
-        console.log("==========================", password.value);
-        console.log("==========================", this.passwordProtection);
+    // check if password is correct
+    const password = await this.getPassword();
+    if (password != this.passwordProtection) {
+      Swal.fire("Incorrect password");
+      return;
+    }
 
-        this.dataTableBody.innerHTML = "";
+    this.dataTableBody.innerHTML = "";
 
-        this._toggleDisplay();
-        this._clearFields();
+    this._toggleDisplay();
+    this._clearFields();
 
-        // this.allDataElem.style.display = "block";
-        // this.appElem.style.display = "none";
-        // this.homeKey.onclick = () => {
-        //   this.allDataElem.style.display = "none";
-        //   this.appElem.style.display = "flex";
-        // };
+    // this.allDataElem.style.display = "block";
+    // this.appElem.style.display = "none";
+    // this.homeKey.onclick = () => {
+    //   this.allDataElem.style.display = "none";
+    //   this.appElem.style.display = "flex";
+    // };
 
-        AllData().then((data) => {
-          // showAlert(this.alertMessage, data);
-          for (let entry of data) {
-            // console.log(entry.website);
-            // console.log(entry.email);
-            // console.log(entry.password);
+    AllData().then((data) => {
+      // showAlert(this.alertMessage, data);
+      for (let entry of data) {
+        // console.log(entry.website);
+        // console.log(entry.email);
+        // console.log(entry.password);
 
-            const row = document.createElement("tr");
-            const websiteCell = document.createElement("td");
-            websiteCell.textContent = entry.website;
-            const emailCell = document.createElement("td");
-            emailCell.textContent = entry.email;
-            const passwordCell = document.createElement("td");
-            passwordCell.textContent = entry.password;
+        const row = document.createElement("tr");
+        const websiteCell = document.createElement("td");
+        websiteCell.textContent = entry.website;
+        const emailCell = document.createElement("td");
+        emailCell.textContent = entry.email;
+        const passwordCell = document.createElement("td");
+        passwordCell.textContent = entry.password;
 
-            row.appendChild(websiteCell);
-            row.appendChild(emailCell);
-            row.appendChild(passwordCell);
-            this.dataTableBody.appendChild(row);
-          }
-        });
+        row.appendChild(websiteCell);
+        row.appendChild(emailCell);
+        row.appendChild(passwordCell);
+        this.dataTableBody.appendChild(row);
       }
     });
   }
 
-  handleSearch() {
+  async handleSearch() {
     const websiteToSearch = this.websiteElement.value;
     if (!websiteToSearch) {
       showAlert(this.alertMessage, "Please enter a website");
       return;
     }
 
-    const { value: password } = Swal.fire({
-      title: "Enter your password",
-      input: "password",
-      inputLabel: "Password",
-      inputPlaceholder: "Enter your password",
-      inputAttributes: {
-        maxlength: "10",
-        autocapitalize: "off",
-        autocorrect: "off",
-      },
-    }).then((password) => {
-      if (password.value != this.passwordProtection) {
-        console.log("==========================", password.value);
-        console.log("==========================", this.passwordProtection);
-        Swal.fire(`Incorrect password`);
+    // check if password is correct
+    const password = await this.getPassword();
+    if (password != this.passwordProtection) {
+      Swal.fire("Incorrect password");
+      return;
+    }
+
+    let itemEmail = "";
+    let itemPassword = "";
+
+    Search(websiteToSearch).then((res) => {
+      if (res[1] == "no") {
+        showAlert(this.alertMessage, "Website doesn't exist");
         return;
       } else {
-        let itemEmail = "";
-        let itemPass = "";
+        itemEmail = res[0].email;
+        itemPassword = res[0].password;
 
-        Search(websiteToSearch).then((res) => {
-          if (res[1] == "no") {
-            showAlert(this.alertMessage, "Website doesn't exist");
-            return;
-          } else {
-            itemEmail = res[0].email;
-            itemPass = res[0].password;
+        const formattedData = `<strong style="user-select: none;">Email:</strong> ${itemEmail} <span id="copy-email" style="cursor: pointer; user-select: none;">&#x1F4CB;</span>
+                    <br><strong style="user-select: none;">Password:</strong> ${itemPassword} <span id="copy-pass" style="cursor: pointer; user-select: none;">&#x1F4CB;</span>`;
 
-            const formattedData = `<strong style="user-select: none;">Email:</strong> ${itemEmail} <span id="copy-email" style="cursor: pointer; user-select: none;">&#x1F4CB;</span>
-                    <br><strong style="user-select: none;">Password:</strong> ${itemPass} <span id="copy-pass" style="cursor: pointer; user-select: none;">&#x1F4CB;</span>`;
-
-            Swal.fire({
-              title: websiteToSearch,
-              html: formattedData,
-              icon: "info",
-            });
-
-            let copyEmail = document.getElementById("copy-email");
-            let copyPass = document.getElementById("copy-pass");
-
-            copyEmail.onclick = function () {
-              copyToClipboard("email", itemEmail);
-              copyEmail.innerHTML = `<span style="background-color: #3498db; color: #fff; padding: 0 10px 0 10px; border-radius: 4px;">Copied!</span>`;
-              setTimeout(() => {
-                copyEmail.innerHTML = "&#x1F4CB;";
-              }, 500);
-            };
-
-            copyPass.onclick = function () {
-              copyToClipboard("password", itemPass);
-              copyPass.innerHTML = `<span style="background-color: #3498db; color: #fff; padding: 0 20px 0 20px; border-radius: 4px;">Copied!</span>`;
-              setTimeout(() => {
-                copyPass.innerHTML = "&#x1F4CB;";
-              }, 500);
-            };
-
-            this._clearFields();
-          }
+        Swal.fire({
+          title: websiteToSearch,
+          html: formattedData,
+          icon: "info",
         });
+
+        let copyEmail = document.getElementById("copy-email");
+        let copyPassword = document.getElementById("copy-pass");
+
+        copyEmail.onclick = function () {
+          copyToClipboard("email", itemEmail);
+          copyEmail.innerHTML = `<span style="background-color: #3498db; color: #fff; padding: 0 10px 0 10px; border-radius: 4px;">Copied!</span>`;
+          setTimeout(() => {
+            copyEmail.innerHTML = "&#x1F4CB;";
+          }, 500);
+        };
+
+        copyPassword.onclick = function () {
+          copyToClipboard("password", itemPassword);
+          copyPassword.innerHTML = `<span style="background-color: #3498db; color: #fff; padding: 0 20px 0 20px; border-radius: 4px;">Copied!</span>`;
+          setTimeout(() => {
+            copyPassword.innerHTML = "&#x1F4CB;";
+          }, 500);
+        };
+
+        this._clearFields();
       }
     });
   }
 
-  handleEdit() {
-    // const { value: password } = Swal.fire({
-    //   title: "Enter your password",
-    //   input: "password",
-    //   inputLabel: "Password",
-    //   inputPlaceholder: "Enter your password",
-    //   inputAttributes: {
-    //     maxlength: "10",
-    //     autocapitalize: "off",
-    //     autocorrect: "off",
-    //   },
-    // }).then((password) => {
-    //   if (password.value != this.passwordProtection) {
-    //     Swal.fire(`Incorrect password`);
-    //     return;
-    //   }
-    // });
-
+  async handleEdit() {
     if (!this.websiteElement.value) {
       showAlert(this.alertMessage, "Enter a website to edit");
+      return;
+    }
+
+    // check if password is correct
+    const password = await this.getPassword();
+    if (password != this.passwordProtection) {
+      Swal.fire("Incorrect password");
       return;
     }
 
@@ -400,6 +350,7 @@ class PasswordManager {
       title: "What do you want to edit?",
       input: "radio",
       inputOptions,
+      showCancelButton: true,
       inputValidator: (value) => {
         if (!value) {
           return "You need to choose something!";
@@ -428,7 +379,6 @@ class PasswordManager {
                   showAlert(this.alertMessage, res);
                 }
               );
-              // Swal.fire("You entered:", `${result}`);
             }
           });
           // * EDIT PASSWORD
